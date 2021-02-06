cc74418b15585870dd34243b73d5a5a3e50c4ae8
Marcelo8173 <marceloandrebio@gmail.com>
2021-02-06 16:55:04 -0300

    add orde by in list course by id

--- src/controller/CourserController.ts
+++ src/controller/CourserController.ts
@@ -44,12 +44,14 @@ class CourserController {
             const ormRepository = getRepository(Courser);
             // const course = await ormRepository.query(`select * from courses where(id = '${id}')`);
             const course = await ormRepository.query(`select
+                l.id,
                 l.name as lessonName,
                 l.duration,
                 l.description,
                 c.name as couseName,
                 c.image
-            from lesson l  inner join courses c on l.course_id = c.id where(c.id = '${id}');`)
+            from lesson l  inner join courses c on l.course_id = c.id where(c.id = '${id}')
+            order by l.created_at;`)
             
             const count = await ormRepository.query(`select count (id) as lessonQtd from lesson;`)
 
